92f367fd74a7238754fa587fae06109cdad6c18d
Andre <andrezio@live.com>
2020-04-18 10:40:51 -0300

    add endpoint

--- app/app.py
+++ app/app.py
@@ -1,10 +1,16 @@
-from flask import Flask, escape, request
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello World'
+
+
+@app.route('/name/<name>')
+def nome(name):
+    return jsonify({"name":name})
+
 
 if __name__ == "__main__":
     app.run()
