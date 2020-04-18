621311f72562faa679a6b2b15913dadfe0144366
Andre Santos Barros da Silva <andrezio@live.com>
2020-04-18 08:43:42 -0300

    update

--- app/app.py
+++ app/app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+    return 'Hello World'
 
 if __name__ == "__main__":
     app.run()
